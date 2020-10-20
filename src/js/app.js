95467f5890fa945f0ff09f73143bdedfb2ec156c
mblaze1986 <morty.blaze@gmail.com>
2020-10-20 14:10:25 +0300

    upd_0200

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ import '../scss/app.scss';
 
 // Your JS Code goes here
 
+$('#burger').click(function (event) {
+  $('#burger').toggleClass('active');
+  $('#nav').toggleClass('active');
+});
+
 let slider = document.getElementById("sliderRange");
 let output = document.getElementById("sliderResult");
 output.innerHTML = slider.value;
@@ -19,6 +24,7 @@ $(document).ready(function () {
       console.log('click');
       const $targetElem = $(this).parent().find(targetMenuSelector);
       $(this).parent().find(targetMenuSelector).toggleClass('hidden').toggleClass('open');
+      $(this).parent().find(btnSelector).toggleClass('open');
       $(document).mouseup(function (e) {
         let container = $targetElem;
         if (container.has(e.target).length === 0) {
@@ -31,6 +37,7 @@ $(document).ready(function () {
     $(this).click(function (event) {
       $(event.currentTarget).parents('.order__menu-collapse').find(btnSelector).html($(event.currentTarget).html() + '.fas fa-angle-down');
       $(event.currentTarget).parents('.order__menu-collapse').find(targetMenuSelector).addClass('hidden');
+      $(event.currentTarget).parents('.order__menu-collapse').find(btnSelector).removeClass('open');
     });
   });
 });
